f5d754036304ac78b3e95a34ef6dd72efc6fac00
admin <user@host.domain>
2026-04-08 10:48:44 -0500

    added part of concept description for variables

--- variables.sh
+++ variables.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-#New Script using Bash for notes and examples of: Variables (setting, calling, echo)
+#This script using Bash is for notes and examples of: Variables (setting, calling, echo)
+
+#CONCEPT DESCRIPTION
+#-------------------
+# Variables are things that we store data within. 
+# They can contain different data types:
+# ex. Integers (int) --> +/- numbers without decimals
+# 	  F
+
+#This is an example of setting a variable. For naming convention: do NOT begin with
+# numbers, capitals, or underscores. Use underscores as spaces. 
+descriptive_variable_name_example="example string of data"
