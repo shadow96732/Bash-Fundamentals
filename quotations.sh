18f25ac01b98dc0a5ec2851b689e8ded43d15686
shadow96732 <itsjustin9732@gmail.com>
2026-04-14 10:30:33 -0500

    created initial concept description. more detail to be added

--- quotations.sh
+++ quotations.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+# This script using Bash is for notes and examples ofthe use of: Quotations
+
+#CONCEPT DESCRIPTION
+# ----------------------------------------------------
+# Quotations are an element used in code to define text as a literal string 
+# of characters - not code that is meant to be executed
