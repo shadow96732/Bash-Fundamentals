9d4c01ca25f74d7768a06b7e326102f7ffd667e7
shadow96732 <itsjustin9732@gmail.com>
2026-04-21 10:45:30 -0500

    added to concept description

--- printf.sh
+++ printf.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+# This script using Bash is for notes and examples ofthe use of: printf()
+# 
+#CONCEPT DESCRIPTION
+# ----------------------------------------------------
+# printf() is a built in command in bash for printing text with formatting options
+
+# echo is the common command for standard output of printing text, but it is more
+# limited in its capabilities and formatting control
