9e4d13cf9a04b53cee7f4ddb94a73b470b5e951f
shadow96732 <itsjustin9732@gmail.com>
2026-04-28 10:19:12 -0500

    added specific comments about [] and (( ))

--- brackets.sh
+++ brackets.sh
@@ -6,8 +6,18 @@
 # Each bracket type is used for different functions
 
 # [] Single Square Brackets
+#	--> used for tests
+#	--> whitespace matters. see ex. below
+if [ 5 > 1 ]; then
+	echo "true"
+fi
 # [[]] Double Square Brackets
 # () Single Parenthesis/Round Brackets
+
 # (()) Double Parenthesis/Round Brackets
+# 	--> Used for evaluating expressions and performing comparisons (arithmetic/math)
+#	--> only uses integers, not decimals
+#	--> does not support strings	
+
 # {} Wavy Brackets/Curly Braces
 # <> Angle Brackets
