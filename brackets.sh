5505dcd1a7b4001d0b4969ef45bdaa79cc8a577f
shadow96732 <itsjustin9732@gmail.com>
2026-04-29 10:30:59 -0500

    elaborated on double square brackets

--- brackets.sh
+++ brackets.sh
@@ -6,18 +6,35 @@
 # Each bracket type is used for different functions
 
 # [] Single Square Brackets
-#	--> used for tests
+#	--> used for testing conditions (comparisons, file existence and attributes)
 #	--> whitespace matters. see ex. below
+#	--> is portable (POSIX)
 if [ 5 > 1 ]; then
 	echo "true"
 fi
+
+if [ -f "file.txt" ]; then
+  echo "File exists"
+else
+	echo "File does not exist"
+fi 
+
 # [[]] Double Square Brackets
+#	--> safer conditionals
+#	--> pattern matching
+#	--> differences from single square brackets
+#		--> supports && and ||
+#		--> supports regex
+#		--> more forgiving with quoting
+
+
 # () Single Parenthesis/Round Brackets
 
 # (()) Double Parenthesis/Round Brackets
 # 	--> Used for evaluating expressions and performing comparisons (arithmetic/math)
 #	--> only uses integers, not decimals
 #	--> does not support strings	
+
 
 # {} Wavy Brackets/Curly Braces
 # <> Angle Brackets
