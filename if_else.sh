92419247b24429d3b9b557eeecef00124f48eedd
shadow96732 <itsjustin9732@gmail.com>
2026-04-30 10:07:19 -0500

    added concept description

--- if_else.sh
+++ if_else.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+# This script using Bash is for notes and examples of the use of: if/elif/else
+
+#CONCEPT DESCRIPTION
+# ----------------------------------------------------
+
+# if, elif, and else are used for what is known as conditional branching 
+# this is when different code is executed depending on whether a condition is met (true/false)
+
+#Common use cases include but are not limited to:
+# --> File and Directory checks
+# --> Comparisons (strings, integers, etc.)
+# --> Command success/failure
+# --> Argument validation
