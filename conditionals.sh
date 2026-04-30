dfdd6071805fdbc552f013a1c22ba7cb837f1bf1
shadow96732 <itsjustin9732@gmail.com>
2026-04-30 10:45:15 -0500

    added content to concept description

---
+++ conditionals.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script using Bash is for notes and examples for the uses of: conditionals
+
+#CONCEPT DESCRIPTION
+# ----------------------------------------------------
+# Conditionals are expressions that evaluate true (0) or false (non-zero) and control program flow
+# 
+# Conditionals vs If/Else
+# --> Conditionals = “making decisions”
+# --> if / else = one specific way to write those decisions
