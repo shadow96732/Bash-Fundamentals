4e49eb2219185f801a49d1adbae72f36f78033e9
shadow96732 <itsjustin9732@gmail.com>
2026-04-30 10:18:53 -0500

    added use cases and ex. code

--- if_else.sh
+++ if_else.sh
@@ -7,8 +7,55 @@
 # if, elif, and else are used for what is known as conditional branching 
 # this is when different code is executed depending on whether a condition is met (true/false)
 
+#Basic Structure:
+
+# if [ condition ]; then
+    # code if condition is true
+# elif [ another_condition ]; then
+    # code if second condition is true
+# else
+    # code if none of the above are true
+# fi
+
 #Common use cases include but are not limited to:
 # --> File and Directory checks
+
+if [ -f "file.txt" ]; then
+    echo "File exists"
+elif [ -d "file.txt" ]; then
+    echo "It's a directory"
+else
+    echo "Not found"
+fi
+
 # --> Comparisons (strings, integers, etc.)
+
+num=10
+
+if [ "$num" -gt 10 ]; then
+    echo "Greater than 10"
+elif [ "$num" -eq 10 ]; then
+    echo "Exactly 10"
+else
+    echo "Less than 10"
+fi
+
 # --> Command success/failure
+
+if grep "hello" file.txt; then
+    echo "Found text"
+else
+    echo "Not found"
+fi
+
 # --> Argument validation
+
+if [ "$#" -eq 0 ]; then
+    echo "No arguments provided"
+elif [ "$#" -eq 1 ]; then
+    echo "One argument provided"
+else
+    echo "Multiple arguments provided"
+fi
+
+
