a63e4bf34223bb2504f30bf1e26ea4a32aaf7d29
shadow96732 <itsjustin9732@gmail.com>
2026-05-01 01:34:30 -0500

    added to concept description

--- conditionals.sh
+++ conditionals.sh
@@ -5,7 +5,11 @@
 #CONCEPT DESCRIPTION
 # ----------------------------------------------------
 # Conditionals are expressions that evaluate true (0) or false (non-zero) and control program flow
-# 
+# this can include (among others):
+# --> if / elif / else → structured branching
+# --> case → pattern-based branching
+# --> [[ ]], [ ], (( )) → condition evaluators
+
 # Conditionals vs If/Else
 # --> Conditionals = “making decisions”
 # --> if / else = one specific way to write those decisions
