68e4f65717d7cf53f76cf53c04b5c2904066b0e9
shadow96732 <itsjustin9732@gmail.com>
2026-05-01 10:21:38 -0500

    added a whole lot to types of conditionals

--- conditionals.sh
+++ conditionals.sh
@@ -5,11 +5,39 @@
 #CONCEPT DESCRIPTION
 # ----------------------------------------------------
 # Conditionals are expressions that evaluate true (0) or false (non-zero) and control program flow
-# this can include (among others):
+# this can include (among others) usage in:
 # --> if / elif / else → structured branching
 # --> case → pattern-based branching
 # --> [[ ]], [ ], (( )) → condition evaluators
 
 # Conditionals vs If/Else
-# --> Conditionals = “making decisions”
-# --> if / else = one specific way to write those decisions
+# --> Conditionals = the question
+# --> if / else = one specific way to write the decision based on answers
+
+#TYPES OF CONDITIONALS
+# ----------------------------------------------------
+# Comparison
+# -->   ==  or -eq (equal)
+# -->   !=  or -ne (not equal)
+# -->   >   or -gt (greater than)
+# -->   <   or -lt (less than)
+# -->   >=  or -ge (greater or equal)
+# -->   <=  or -le (less or equal)
+
+year=2026
+if [ "$year" == 2026 ]; then #the condition is that the variable $year must be equal to the value 2026
+	echo "year is 2026"
+fi
+
+# Logical
+# --> AND (&&)
+# --> OR (||)
+# --> NOT (!)
+
+# File Checks
+# -->  -f file   regular file
+# -->  -d dir    directory
+# -->  -e path   exists
+# -->  -r file   readable
+# -->  -w file   writable
+# -->  -x file   executable
