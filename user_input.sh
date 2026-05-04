ff4b7f69e2eace898b61e7eacdf365c200167b0a
shadow96732 <itsjustin9732@gmail.com>
2026-05-04 10:25:01 -0500

    added common mistakes

--- user_input.sh
+++ user_input.sh
@@ -10,6 +10,7 @@
 # --> The newline is removed
 # --> Data goes into one or more variables
 # 	--> extra values go into the last variable
+#	--> if not enough values are supplied, the extra variables remain empty
 # --> if no variable is given, it defaults storage in $REPLY
 
 # FLAGS
@@ -24,5 +25,17 @@ echo "your input was: $REPLY"
 
 read -p "Enter First name and Last name: " first_name last_name
 read -s -p "Enter a password here: " password
+while [ "$password" == "password" ]; do
+	echo "pick a different password... thats not secure"
+	read -p "enter a proper password: " password
+done
 echo -e " \n Your first name is: $first_name \n your last name is: $last_name \n the password you entered was: $password"
 
+#COMMON MISTAKES
+# -->Not specifying a given variable or not using the default $REPLY accordingly.
+# -->Accidentally overwriting variables
+#	--> reusing a variable is acceptable, on the condition that you do not want to reuse the old value.
+# --> Improper bracket, quotation or condition usage (see brackets.sh OR conditionals.sh OR quotations.sh for more detail)
+# 		--> variables should be quoted if using single []
+#		--> int and str have different comparison operators
+#		--> single quotes do not support variable expansion
