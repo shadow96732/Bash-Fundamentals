a70852c8828805b706f5ca1ada0435a50f7b5d8a
shadow96732 <197800308+shadow96732@users.noreply.github>
2026-05-06 10:21:21 -0500

    added ex. code

--- arrays.sh
+++ arrays.sh
@@ -4,3 +4,8 @@
 #CONCEPT DESCRIPTION
 # ----------------------------------------------------
 # An array is a variable that holds multiple values
+# --> they are indexed (ordered) with the index starting from 0
+
+array=("value1" "value2" "value3")
+
+echo "${array[1]}" # the value within the [] is the index being called
