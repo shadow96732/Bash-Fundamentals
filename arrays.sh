cbdac0d81c9299ae6397a0cf691607cd1c53995f
shadow96732 <197800308+shadow96732@users.noreply.github>
2026-05-06 10:11:36 -0500

    added concept desc

---
+++ arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script using Bash is for notes and examples for the use of: arrays
+
+#CONCEPT DESCRIPTION
+# ----------------------------------------------------
+# An array is a variable that holds multiple values
