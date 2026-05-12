985acd0cdca0cbbdb879444c8f6059690eaba7bb
shadow96732 <197800308+shadow96732@users.noreply.github>
2026-05-12 10:23:39 -0500

    added common mistakes

--- functions.sh
+++ functions.sh
@@ -6,7 +6,17 @@
 # Functions are organized blocks of code that do a specific task. 
 # They help organize code, reduce repetition, and enable easy reuse of code.
 
+# Bash has built-in functions already as well, such as cd (change directory)
+# --> echo, read, printf, etc. are built-in *commands not functions
+
+# --> function declaration includes the function name, parentheses for parameters
+# --> all commands in the function should be enclosed within curly braces {}
 function_name() {
 	echo "commands go here"
 }
 function_name #this is the function call
+
+# COMMON MISTAKES
+# --> forgetting () in declaration
+# --> calling the function prior to its declaration
+# --> calling the you are trying to delcare within the declaration
