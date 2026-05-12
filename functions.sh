58a99e70453ce65eef80894c2b5ea9cc11e9660b
shadow96732 <197800308+shadow96732@users.noreply.github>
2026-05-12 10:03:10 -0500

    added ex. code

--- functions.sh
+++ functions.sh
@@ -3,3 +3,10 @@
 
 #CONCEPT DESCRIPTION
 # ----------------------------------------------------
+# Functions are organized blocks of code that do a specific task. 
+# They help organize code, reduce repetition, and enable easy reuse of code.
+
+function_name() {
+	echo "commands go here"
+}
+function_name #this is the function call
