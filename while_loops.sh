17965b68f93d17f35c52c0f4a0e0c2cc6689b728
shadow96732 <197800308+shadow96732@users.noreply.github>
2026-05-13 10:29:39 -0500

    added ex. code

--- while_loops.sh
+++ while_loops.sh
@@ -3,3 +3,15 @@
 
 #CONCEPT DESCRIPTION
 # ----------------------------------------------------
+# A while loop repeatedly runs code while a condition is true
+
+read -p "enter a value for x so that 99 + x = 100:
+Enter here: " while_loop_variable_input
+answer=$((99+while_loop_variable_input))
+
+while [[ $answer != 100 ]]; do
+
+	read -p 'sum of numbers doesnt equal 100. Enter a different answer: ' while_loop_variable_input
+	answer=$((99+while_loop_variable_input))
+	
+done
