504b07789f2a2814d1134c7cf00911dc141764fa
shadow96732 <197800308+shadow96732@users.noreply.github>
2026-05-13 10:21:12 -0500

    added shebang and general concept comment

--- while_loops.sh
+++ while_loops.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script using Bash is for notes and examples for the use of: default values
+# This script using Bash is for notes and examples for the use of: while loops
 
 #CONCEPT DESCRIPTION
 # ----------------------------------------------------
