63ceae62132994a7c5c29163b0d4d311e9c52cf2
shadow96732 <197800308+shadow96732@users.noreply.github>
2026-05-14 10:38:07 -0500

    added concept desc

---
+++ pipes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script using Bash is for notes and examples for the use of: pipes |
+
+#CONCEPT DESCRIPTION
+# ----------------------------------------------------
+# Pipes in Bash are like a conveyor belt
+# --> they take the standard output of a command and put it into another command
+#	--> General form: command1 | command2
+#			VISUAL MENTAL MODEL
+#				--> without pipe: command --> screen
+#				--> with pipe: command1 --> command2 --> screen
