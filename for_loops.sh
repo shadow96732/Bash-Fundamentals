c827db998824e9638d157bf739b0897ead71dca9
shadow96732 <197800308+shadow96732@users.noreply.github.com>
2026-05-15 00:53:34 -0500

    added concept description

---
+++ for_loops.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script using Bash is for notes and examples for the use of: for loops
+
+#CONCEPT DESCRIPTION
+# ----------------------------------------------------
+# for loops in bash are used to repeat commands for a list of values
