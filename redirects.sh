df5c8b396849e3f968aa3a09a49526b4eec5364b
shadow96732 <197800308+shadow96732@users.noreply.github.com>
2026-05-15 00:40:50 -0500

    added specific redirects

--- redirects.sh
+++ redirects.sh
@@ -11,6 +11,7 @@
 # --> sends output(s) of a command into a file
 # 	--> creates file if its missing
 #	--> OVERWRITES existing files
+
 echo "Hello world" > file.txt && cat file.txt
 echo "overwrite file" > file.txt #&& cat file.txt
 
@@ -22,3 +23,26 @@ echo "append redirect" >> file.txt && cat file.txt
 # Input Redirection (<)
 # --> uses file as input
 wc -l < file.txt
+
+
+# FILE DESCRIPTORS
+# --> 0 stdin (input)
+# --> 1 stdout (normal output)
+# --> 2 stderr (error output)
+# this can be used to control specific components
+
+ls missingfile 2> errors.txt #redirects errors only
+
+echo "separate redirects" > file.txt 2> errors.txt 
+cat output.txt && cat errors.txt
+# --> this would be for redirecting stdout and stderr separately (if there was errors)
+
+echo "redirect together" &> all.txt
+# Note: this is the modern Bash syntax
+# the portable syntax is:
+echo "redirect together" > all.txt 2>&1 #this means that stderr is directed to wherever stdout is going
+# Note: the order matters. the redirect is placed after the location
+
+# * discarding output
+# --> it is very common to use the "black hole" file when you dont want outputs to be displayed
+#	--> you would redirect commands to /dev/null
