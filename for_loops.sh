d7760a5c65d986a75c5c23538d597d0550738ddf
shadow96732 <197800308+shadow96732@users.noreply.github.com>
2026-05-15 01:03:17 -0500

    added ex. code

--- for_loops.sh
+++ for_loops.sh
@@ -4,3 +4,22 @@
 #CONCEPT DESCRIPTION
 # ----------------------------------------------------
 # for loops in bash are used to repeat commands for a list of values
+
+for i in {1..5}; do
+  echo "$i"
+done
+
+# another example for arrays
+arr=("red" "green" "blue")
+
+for color in "${arr[@]}"; do
+  echo "$color"
+done
+
+# the differences between for and while
+# --> for is best used for known lists, while 'while' is for unknown durations
+# --> for uses an iteration structure, whereas while is condition based
+
+# practical usage (batch renaming example)
+for file in *.txt; do
+	mv "$file" "$file.sh" 
